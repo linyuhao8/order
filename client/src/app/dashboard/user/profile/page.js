44b18b306c990bbee0aac27b4edd9b002ab46121
linyuhao8 <linhao8896@gmail.com>
2025-03-25 12:01:31 +0800

    Profile Page redirect to Profile ID Page or Login

---
+++ client/src/app/dashboard/user/profile/page.js
@@ -0,0 +1,10 @@
+import { redirect } from "next/navigation";
+import { api } from "@/api";
+export default async function ProfilePage() {
+  const res = await api.auth.checkAuth();
+  if (!res.ok) {
+    redirect("/login");
+  }
+  const user = await res.json();
+  redirect(`/dashboard/user/profile/${user.id}`); // redirect to profile page
+}

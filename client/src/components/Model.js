a97a85f7e5b5f14f5f2908472229e77eee501aaa
linyuhao8 <linhao8896@gmail.com>
2025-03-28 15:00:31 +0800

    Add Model component and Model hook

---
+++ client/src/components/Model.js
@@ -0,0 +1,20 @@
+// Modal.js
+import React from "react";
+
+export default function Modal({ isOpen, closeModal, children }) {
+  if (!isOpen) return null;
+
+  return (
+    <div
+      className="fixed inset-0 bg-black/70 flex justify-center items-center w-[auto] h-[auto] z-50"
+      onClick={closeModal}
+    >
+      <div
+        className="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-lg transform transition-all scale-100 opacity-100 duration-300 ease-in-out"
+        onClick={(e) => e.stopPropagation()}
+      >
+        {children}
+      </div>
+    </div>
+  );
+}
---
+++ client/src/hooks/ui/useModel.js
@@ -0,0 +1,15 @@
+// useModal.js
+import { useState } from "react";
+
+export default function useModal() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
+  return {
+    isModalOpen,
+    openModal,
+    closeModal,
+  };
+}

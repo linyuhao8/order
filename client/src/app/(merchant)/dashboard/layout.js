c833bac74b6ef54a1d4f04b3c7e445cb41b398a8
linyuhao8 <linhao8896@gmail.com>
2025-03-28 17:22:45 +0800

    Make (merchant) and (public) use common redux and share globals.css

--- client/src/app/(merchant)/dashboard/layout.js
+++ client/src/app/(merchant)/dashboard/layout.js
@@ -1,23 +1,17 @@
-import "@/styles/globals.css";
 import Navbar from "@/components/dashboard/Navbar";
-import { StoreProvider } from "@/lib/storeProvide";
 
 export const metadata = {
   title: "dashboard",
-  description: "dash",
+  description: "dashboard",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>
-        <div className="dashboard min-h-screen dark:bg-gray-900">
-          <div className="flex flex-col md:flex-row">
-            <Navbar />
-            <StoreProvider>{children}</StoreProvider>
-          </div>
-        </div>
-      </body>
-    </html>
+    <div className="dashboard min-h-screen dark:bg-gray-900">
+      <div className="flex flex-col md:flex-row">
+        <Navbar />
+        {children}
+      </div>
+    </div>
   );
 }
--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,10 +1,11 @@
+//app/layout.js For merchant and public
 import "@/styles/globals.css";
-import { StoreProvider } from "../../lib/storeProvide";
+import { StoreProvider } from "@/lib/storeProvide";
 import ToastProvider from "@/components/ToastProvider";
 
 export const metadata = {
   title: "Order",
-  description: "點餐平台",
+  description: "order",
 };
 
 export default function RootLayout({ children }) {
@@ -12,6 +13,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         <StoreProvider>
+          {/* Redux Store */}
           {children}
           <ToastProvider /> {/* Only run on Csr */}
         </StoreProvider>

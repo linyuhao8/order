0a3e3c1ae5f6b09462f2793055600ba5a1c1a295
linyuhao8 <linhao8896@gmail.com>
2025-03-28 15:31:41 +0800

    client/ profile/page useAuth to redirect

--- client/src/app/dashboard/user/profile/page.js
+++ client/src/app/dashboard/user/profile/page.js
@@ -1,11 +1,23 @@
-import { redirect } from "next/navigation";
-import { api } from "@/api";
-export default async function ProfilePage() {
-  const res = await api.auth.checkAuth();
-  if (!res.ok) {
-    redirect("/login");
-  }
-  const user = await res.json();
-  redirect(`/dashboard/user/profile/${user.id}`); // redirect to profile page
-}
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+//hook
 import useAuth from "@/hooks/auth/useAuth";
+
+const ProfilePage = () => {
+  const router = useRouter();
+  // If authentication fails, it will automatically redirect to the login page.
+  const { isAuthenticated, user } = useAuth(true);
+
+  useEffect(() => {
+    // If the validation is successful, jump to the corresponding Profile page.
+    if (isAuthenticated && user) {
+      router.push(`/dashboard/user/profile/${user.id}`);
+    }
+  }, [isAuthenticated, user, router]);
+
+  return <div className="w-full md:ml-64 p-4 md:p-6">Loading...</div>; // 加載頁面時顯示 Loading
+};
+
+export default ProfilePage;

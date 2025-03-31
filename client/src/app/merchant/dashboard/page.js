99f24d2694748f624877feadf6ba6ba7e5e6b571
linyuhao8 <linhao8896@gmail.com>
2025-03-31 13:38:50 +0800

    Splitting the dashboard header into components

--- client/src/app/merchant/dashboard/page.js
+++ client/src/app/merchant/dashboard/page.js
@@ -1,84 +1,13 @@
 "use client";
-//icon
-import { IoIosSettings } from "react-icons/io";
-import { FaCloudSun } from "react-icons/fa";
-import { MdDarkMode } from "react-icons/md";
-
-//redux
-import { toggleTheme } from "@/lib/slices/themeSlice";
-import { useSelector, useDispatch } from "react-redux";
-
-//hook
-import useThemeSwitcher from "@/hooks/ui/useThemeSwitcher";
 import withAuth from "@/hoc/withAuth";
-
-//model
-import { Modal, SubModal } from "@/components/common/Model";
-import useModel from "@/hooks/ui/useModel";
-import SettingPage from "./settings/page";
+import Header from "@/components/merchant/dashboard/Header";
 
 function Dashboard() {
-  const theme = useSelector((state) => state.theme.mode);
-  const dispatch = useDispatch();
-
-  const [isModalOpen, openModal, closeModal] = useModel();
-  const [isSubModalOpen, openSubModal, closeSubModal] = useModel();
-
-
-  useThemeSwitcher(theme);
-
   return (
     <>
       {/* Main Content - Adjusted for Responsiveness */}
       <div>
-        <header className="flex flex-col md:flex-row justify-between items-start md:items-center mb-8 space-y-4 md:space-y-0">
-          <h1 className="text-2xl font-semibold dark:text-white">
-            Merchant Dashboard
-          </h1>
-          <div className="flex flex-wrap items-center gap-4">
-            <div className="relative flex-grow max-w-xs">
-              <i className="fas fa-search absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500"></i>
-              <input
-                type="text"
-                placeholder="Search"
-                className="w-full pl-10 pr-4 py-2 rounded-lg border border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100"
-              />
-            </div>
-            <div className="flex items-center space-x-2">
-              <button
-                onClick={() => dispatch(toggleTheme())}
-                className="w-10 h-10 rounded-full flex items-center justify-center bg-gray-100 dark:bg-gray-700 text-gray-600 dark:text-gray-300 cursor-pointer"
-              >
-                {theme === "dark" ? <MdDarkMode /> : <FaCloudSun />}
-              </button>
-              <div>
-                <button onClick={openModal}>
-                  Open Main Modal
-                </button>
-
-                <Modal
-                  isOpen={isModalOpen}
-                  closeModal={closeModal}
-                >
-                  <SettingPage />
-                  <button onClick={openSubModal}>
-                    Open SubModal
-                  </button>
-                </Modal>
-
-                <SubModal
-                  isOpen={isSubModalOpen}
-                  closeModal={closeSubModal}
-                >
-                  <h3>Sub Modal</h3>
-                </SubModal>
-              </div>
-              {/* 父級 Modal */}
-
-              <IoIosSettings className="text-2xl" />
-            </div>
-          </div>
-        </header>
+        <Header />
         {/* Grid Layout with Responsive Columns */}
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           {/* Employee Profile Card */}
@@ -159,7 +88,7 @@ function Dashboard() {
                     cy="90"
                     r="75"
                     fill="none"
-                    stroke={theme === "dark" ? "#374151" : "#f3f4f6"}
+                    // stroke={theme === "dark" ? "#374151" : "#f3f4f6"}
                     strokeWidth="12"
                   />
                   <circle

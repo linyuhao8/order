d730f8579998eec2e651fece2e539d6ac22872cf
linyuhao8 <linhao8896@gmail.com>
2025-04-12 12:20:09 +0800

    feat(client): Loading style

--- client/src/components/common/Loading.js
+++ client/src/components/common/Loading.js
@@ -4,7 +4,7 @@ const Loading = () => {
   return (
     <div className="flex flex-col items-center justify-center h-full py-10">
       <p className="text-gray-600 mb-4">Loading...</p>
-      <div className="w-8 h-8 border-4 border-t-blue-500 border-gray-200 rounded-full animate-spin" />
+      <div className="w-8 h-8 border-4 border-t-amber-500 border-gray-200 rounded-full animate-spin" />
     </div>
   );
 };

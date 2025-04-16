8f81efc9055beffac0dd7c0bd416d9cbe50cbd62
linyuhao8 <linhao8896@gmail.com>
2025-04-16 14:22:08 +0800

    feat(server): merchantCategoryValidation midify

--- server/validations/user/merchant/merchantCategoryValidation.js
+++ server/validations/user/merchant/merchantCategoryValidation.js
@@ -7,7 +7,7 @@ const createCategorySchema = Joi.object({
     "string.empty": "分類名稱不能為空",
   }),
   description: Joi.string().allow("").optional(),
-  img: Joi.string().uri().allow(null).optional().messages({
+  img: Joi.string().uri().allow(null,'').optional().messages({
     "string.uri": "圖片 URL 格式不正確",
   }),
   merchant_ids: Joi.array().items(Joi.number().integer()).optional(),
@@ -17,15 +17,15 @@ const createCategorySchema = Joi.object({
 const updateCategorySchema = Joi.object({
   name: Joi.string().trim().optional(),
   description: Joi.string().allow("").optional(),
-  img: Joi.string().uri().optional(),
+  img: Joi.string().uri().allow(null, "").optional(),
   merchant_ids: Joi.array().items(Joi.number().integer()).optional(),
 });
 
 // 新增商家到分類的驗證規則
 const addCategoryToMerchantSchema = Joi.object({
-  merchant_id: Joi.number().integer().required().messages({
+  merchant_id: Joi.string().guid({ version: 'uuidv4' }).required().messages({
     "any.required": "商家 ID 為必填",
-    "number.base": "商家 ID 必須為數字",
+    "string.guid": "user_id 必須是有效的 UUID",
   }),
   category_id: Joi.number().integer().required().messages({
     "any.required": "分類 ID 為必填",

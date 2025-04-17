e92e2cd31fe0f9b60e956606f47649c2e7870c97
linyuhao8 <linhao8896@gmail.com>
2025-04-17 17:33:44 +0800

    feat(server): allow empty string and null for optional merchant fields

--- server/validations/user/merchant/merchantValidation.js
+++ server/validations/user/merchant/merchantValidation.js
@@ -10,8 +10,8 @@ const createMerchantValidation = Joi.object({
     "string.max": "商家名稱最多 255 字元",
     "any.required": "商家名稱是必填欄位",
   }),
-  description: Joi.string().optional(),
-  feature: Joi.string().max(10).optional().messages({
+  description: Joi.string().allow("").optional(),
+  feature: Joi.string().max(10).allow("").optional().messages({
     "string.max": "商家特色最多 10 字元",
   }),
   location: Joi.string().allow("").optional(),
@@ -20,6 +20,7 @@ const createMerchantValidation = Joi.object({
   }),
   merchant_logo_id: Joi.string()
     .guid({ version: "uuidv4" })
+    .allow("", null)
     .optional()
     .messages({
       "string.guid": "merchant_logo_id 必須是有效的 UUID",
@@ -31,23 +32,29 @@ const createMerchantValidation = Joi.object({
 
 // 用於更新商家 (PUT)
 const updateMerchantValidation = Joi.object({
-  business_name: Joi.string().max(255).optional().messages({
-    "string.max": "商家名稱最多 255 字元",
+  user_id: Joi.string().guid({ version: "uuidv4" }).required().messages({
+    "string.guid": "user_id 必須是有效的 UUID",
+    "any.required": "user_id 是必填欄位",
   }),
-  description: Joi.string().optional(),
-  feature: Joi.string().max(10).optional().messages({
+  business_name: Joi.string().max(255).required().messages({
+    "string.max": "商家名稱最多 255 字元",
+    "any.required": "商家名稱是必填欄位",
+  }),
+  description: Joi.string().allow("").optional(),
+  feature: Joi.string().max(10).allow("").optional().messages({
     "string.max": "商家特色最多 10 字元",
   }),
-  merchant_logo_id: Joi.string()
-    .guid({ version: "uuidv4" })
-    .optional()
-    .messages({
-      "string.guid": "merchant_logo_id 必須是有效的 UUID",
-    }),
   location: Joi.string().allow("").optional(),
   image_id: Joi.string().guid({ version: "uuidv4" }).optional().messages({
     "string.guid": "image_id 必須是有效的 UUID",
   }),
+  merchant_logo_id: Joi.string()
+    .guid({ version: "uuidv4" })
+    .allow("", null)
+    .optional()
+    .messages({
+      "string.guid": "merchant_logo_id 必須是有效的 UUID",
+    }),
   business_hours: Joi.string().max(100).optional().messages({
     "string.max": "business_hours 最多 100 個字",
   }),

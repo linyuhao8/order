07021e18747672c075230a29839991d9b4feae04
linyuhao8 <linhao8896@gmail.com>
2025-06-12 12:06:18 +0800

    feat(client/add-merchantpage): add layout

---
+++ client/src/app/merchant/dashboard/add-merchant/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "Add merchant",
+  description: "Add merchant Page",
+};
+
+export default async function AddMerchantPageLayout({ children }) {
+  return <>{children}</>;
+}

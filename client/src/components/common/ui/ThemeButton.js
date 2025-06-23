7dac645f51997858354d04b8d5cfe012fb393947
linyuhao8 <linhao8896@gmail.com>
2025-06-23 14:31:36 +0800

    Feat(client): Add icon style

--- client/src/components/common/ui/ThemeButton.js
+++ client/src/components/common/ui/ThemeButton.js
@@ -13,7 +13,7 @@ import { useSelector, useDispatch } from "react-redux";
 // Import custom hook for handling theme switching
 import useThemeSwitcher from "@/hooks/ui/useThemeSwitcher";
 
-const ThemeButton = () => {
+const ThemeButton = ({ variant = "icon", text = false }) => {
   // Initialize Redux dispatch function
   const dispatch = useDispatch();
 
@@ -22,11 +22,18 @@ const ThemeButton = () => {
 
   // Apply the theme switcher effect based on the current theme
   useThemeSwitcher(theme);
-
+  const isIconOnly = !text;
+  const label = theme === "dark" ? "深色" : "淺色";
   return (
     // Button to toggle the theme between light and dark mode
-    <Button onClick={() => dispatch(toggleTheme())} variant="icon" size="icon">
+    <Button
+      onClick={() => dispatch(toggleTheme())}
+      variant={variant}
+      size={isIconOnly ? "icon" : "md"}
+      className={`flex items-center ${!isIconOnly ? "gap-2" : ""}`}
+    >
       {theme === "dark" ? <MdDarkMode /> : <FaCloudSun />}
+      {!isIconOnly && <span>{label}</span>}
     </Button>
   );
 };
